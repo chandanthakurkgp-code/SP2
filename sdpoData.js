d832411cb9c6a6686ea924cff9b9a1d575dd8c30
chandanthakurkgp-code <chandan.thakurkgp@gmail.com>
2026-01-22 00:27:53 +0530

    Add files via upload

--- sdpoData.js
+++ sdpoData.js
@@ -2,9 +2,9 @@ const sdpoData = {
     "sdpo": {
         "title": "SDPO",
         "partners": [
-            {"name": "SI Arun Gope", "address": "Kharagpur Town PS", "mobile": "9876543210", "whatsapp": "919876543210", "dob": "1985-05-15", "doj": "2010-01-20", "dor": "2045-05-31"},
-            {"name": "SI Sourav Sarkar", "address": "Kharagpur Town PS", "mobile": "9876543215", "whatsapp": "919876543215", "dob": "1988-08-10", "doj": "2012-03-12", "dor": "2048-08-31"},
-            {"name": "SI Abir Alam", "address": "Khirpai PS", "mobile": "9876500000", "whatsapp": "919876500000", "dob": "1990-12-01", "doj": "2015-06-25", "dor": "2050-11-30"}
+            { "name": "SI Arun Gope", "address": "Kharagput Town PS", "mobile": "9876543210", "whatsapp": "919876543210" },
+            { "name": "SI Sourav Sarkar", "address": "Kharagput Town PS", "mobile": "9876543215", "whatsapp": "919876543215" },
+            { "name": "SI Abir Alam", "address": "Khirpai PS", "mobile": "9876500000", "whatsapp": "919876500000" }
         ]
     }
 };
