6171710bd668302921355787a16a910497a7c490
chandanthakurkgp-code <chandan.thakurkgp@gmail.com>
2026-01-21 22:54:40 +0530

    Add files via upload

--- sdpoData.js
+++ sdpoData.js
@@ -8,6 +8,7 @@ const sdpoData = {
                 "mobile": "9876543210",
                 "whatsapp": "919876543210"
             },
+            {"name": "SI Sourav Sarkar","address": "Kharagput Town PS","mobile": "9876543215","whatsapp": "919876543215"},
             {
                 "name": "SI Abir Alam",
                 "address": "Khirpai PS",
